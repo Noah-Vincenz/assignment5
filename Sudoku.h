90078777eb19477e2d3c555a0d78fc30a2928925
Noah-Vincenz Noeh <noah-vincenz.noeh@kcl.ac.uk>
2016-11-06 23:51:09 +0000

    -

--- Sudoku.h
+++ Sudoku.h
@@ -7,7 +7,7 @@
 
 
 // TODO: Your Sudoku class goes here:
-class Sudoku {
+class Sudoku /*: public Searchable */ {
 private:
     int size;
     vector<vector<std::set<int> > > field;
@@ -38,11 +38,10 @@ public:
     }
 
     bool setSquare(int row, int col, int value) {
-        std::cout << "setting a sqr." << std::endl;
         bool result = true;
         field[row][col].clear();
         field[row][col].insert(value);
-        std::cout << "Row: " << row << "  Col: " << col << "  Data: " << getSquare(row, col) << std::endl;
+        std::cout << "Setting a sqr: Row: " << row << "  Col: " << col << "  Data: " << getSquare(row, col) << std::endl;
         bool isStillChanging = true;
         while (isStillChanging) {
             isStillChanging = false;
@@ -53,7 +52,6 @@ public:
                         int value = field[row1][col1].begin().operator*();
                         for (int i = 0; i < size; ++i) {
                             //taking care of squares on same row
-                            //if (field[row1][i].find(value).operator*() == value && field[row1][i].size() != 1) { //if value is inside the set && value is not only value
                             if (field[row1][i].find(value).operator*() == value && i != col1) { //if value is inside the set && value is not only value
                                 field[row1][i].erase(value); //does erase erase at index or erase that elem
                                 isStillChanging = true;
@@ -65,7 +63,6 @@ public:
                                 std::cout << "size: " << field[row1][i].size() << std::endl;
                             }
                             //taking care of squares on same column
-                            //if (field[i][col1].find(value).operator*() == value && field[i][col1].size() != 1) { //if value is inside the set && value is not only value
                             if (field[i][col1].find(value).operator*() == value && i != row1) { //if value is inside the set && value is not only value
                                 field[i][col1].erase(value);
                                 isStillChanging = true;
@@ -82,7 +79,6 @@ public:
                         int colMod = col1 - col1 % sqrt;
                         for (int row2 = rowMod; row2 < (rowMod + sqrt); ++row2) {
                             for (int col2 = colMod; col2 < (colMod + sqrt); ++col2) {
-                                //if (field[row2][col2].find(value).operator*() == value && field[row2][col2].size() != 1) { //if value is inside the set && value is not only value
                                 if (field[row2][col2].find(value).operator*() == value && row2 != row1 && col2 != col1) { //if value is inside the set && value is not only value
                                     field[row2][col2].erase(value);
                                     isStillChanging = true;
@@ -90,8 +86,7 @@ public:
                                         result = false;
                                         break;
                                     }
-                                    std::cout << "erasing " << value << " from (" << row2 << ", " << col2 << ")."
-                                              << std::endl;
+                                    std::cout << "erasing " << value << " from (" << row2 << ", " << col2 << ")." << std::endl;
                                     std::cout << "size: " << field[row2][col2].size() << std::endl;
                                 }
                             }
@@ -102,5 +97,40 @@ public:
         }
         return result;
     }
+    bool isSolution() {
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                if (field[i][j].size() != 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    void write(ostream & o) { //std::ostream??
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                if (getSquare(i,j) == -1) {
+                    o << " ";
+                }
+                else {
+                    o << getSquare(i,j);
+                }
+            }
+        }
+    }
+    /*
+    Sudoku successors() {
+        vector<unique_ptr<Searchable> > successors;
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                if (getSquare(i,j) == -1) {
+                    int row = i;
+                    int leftMostSquare = getSquare(row,j);
+                }
+            }
+        }
+    }
+    */
 };
 #endif
