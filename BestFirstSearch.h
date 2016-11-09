20b540c74dcb10b25ebe1a19e5c7912e7b52256a
Noah-Vincenz Noeh <noah-vincenz.noeh@kcl.ac.uk>
2016-11-09 19:43:07 +0000

    done

--- BestFirstSearch.h
+++ BestFirstSearch.h
@@ -4,7 +4,12 @@
 #include "Searchable.h"
 
 #include <memory>
+#include <queue>
+#include <deque>
+using std::deque;
+
 using std::unique_ptr;
+
 
 class BestFirstSearch {
 
@@ -12,14 +17,19 @@ protected:
 
     /// Make sure you increment this every time you 'expand' a node, by getting it successors and putting them on the queue        
     int nodes = 0;
-    
-    
+    deque<unique_ptr<Searchable> > Q;
+//    std::priority_queue<unique_ptr<Searchable>, std::vector<unique_ptr<Searchable> >, ComparatorA> myQ;
+
+
+
 public:
 
     BestFirstSearch(std::unique_ptr<Searchable> && startFrom) {
         // TODO Put startFrom onto the queue:
-        
+        myQ.push(std::move(startFrom));
+
     }
+
     
     int getNodesExpanded() const {
         return nodes;
@@ -28,10 +38,79 @@ public:
     Searchable * solve() {
             
         // TODO Implement search, returning a pointer to the solution (if one is found)
-    
+        while (!Q.empty()) { //will be 1 at the beginning
+
+
+
+            // If the solution on the front of the queue is a solution
+            if (Q.front()->isSolution()) {
+                return Q.front().get(); // return the pointer
+            }
+
+
+            ++nodes; // we've now looked at one more node, increment our counter
+
+            // Steal the pointer to the board at the front of the queue, by moving it into a unique_ptr here
+            // After this, the pointer on the front of the queue is `nullptr`...
+            unique_ptr<Searchable> current(std::move(Q.front()));
+
+            // ...which can then be popped off the front of the queue
+            Q.pop_front(); //elems are poppped from back / top
+
+            // Get the successors...
+            vector<unique_ptr<Searchable>> successors = current->successors();
+
+
+            for (auto & successor : successors) {
+                // and push each one onto the back of queue.  --------------here it needs to be changed; succerssors need to be added in ascending order of their heuristicValue
+                myQ.push(std::move(successor));
+
+            }
+        }
+        /*
+            for (auto & successor : successors) {
+                // and push each one onto the back of queue.
+                bool emplaced = false;
+                auto itr = Q.begin();
+                unique_ptr<Searchable> tempQ;
+                tempQ.reset(*itr);
+                //unique_ptr<Searchable> * tempQ = new unique_ptr<Searchable>(*itr);
+                for (int i = 0; i < Q.size(); ++i) {
+                    if (successor.get()->heuristicValue() <= tempQ.get()->heuristicValue()) {
+                        Q.emplace(itr, std::move(successor));
+                        emplaced = true;
+                    }
+                    else {
+                        ++itr;
+                        tempQ = *itr;
+                    }
+                }
+                if (emplaced == false) {
+                    Q.emplace_back(std::move(successor));
+                }
+            }
+        }
+*/
+
+
+
+
+
+
         return nullptr;
     }
 };
+/*
+class ComparatorA
+{
+public:
+    bool operator() (const unique_ptr<Searchable> s1, const unique_ptr<Searchable> s2)
+    {
+        return s1.get()->heuristicValue()<s2.get()->heuristicValue();
+    }
+};
+ */
+
 
 
 // Do not edit below this line
--- Sudoku.h
+++ Sudoku.h
@@ -43,10 +43,13 @@ public:
             isStillChanging = false;
             for (int row1 = 0; row1 < size; ++row1) {
                 for (int col1 = 0; col1 < size; ++col1) {
+                    int sqrt = std::sqrt(size);
+                    int rowMod = row1 - row1 % sqrt;
+                    int colMod = col1 - col1 % sqrt;
                     if (field[row1][col1].size() == 1) {
                         int value = *field[row1][col1].begin();
                         for (int i = 0; i < size; ++i) {
-                            //taking care of squares on same row
+                            //taking care of squares on same col
                             if (field[i][col1].find(value) != field[i][col1].end() && i != row1) { //if value is inside the set && value is not only value
                                 field[i][col1].erase(value);
                                 isStillChanging = true;
@@ -55,7 +58,7 @@ public:
                                     break;
                                 }
                             }
-                            //taking care of squares on same column
+                            //taking care of squares on same row
                             if (field[row1][i].find(value) != field[row1][i].end() && i != col1) { //if value is inside the set && value is not only value
                                 field[row1][i].erase(value);
                                 isStillChanging = true;
@@ -65,9 +68,6 @@ public:
                                 }
                             }
                         }
-                        int sqrt = std::sqrt(size);
-                        int rowMod = row1 - row1 % sqrt;
-                        int colMod = col1 - col1 % sqrt;
                         for (int row2 = rowMod; row2 < (rowMod + sqrt); ++row2) {
                             for (int col2 = colMod; col2 < (colMod + sqrt); ++col2) {
                                 if (field[row2][col2].find(value) != field[row2][col2].end() && row2 != row1 && col2 != col1) { //if value is inside the set && value is not only value
@@ -81,24 +81,83 @@ public:
                             }
                         }
                     }
+                    //checking for identical pairs in sets
+                    else if (field[row1][col1].size() == 2) {
+                        for (int i = 0; i < size; ++i) {
+                            if (field[row1][col1] == field[row1][i] && i != col1) { //if the sets are the same
+                                for (int j = 0; j < size && j != col1 && j != i; ++j) {
+                                    for (int k : field[row1][col1]) {
+                                        if (field[row1][j].find(k) != field[row1][j].end()) {
+                                            field[row1][j].erase(k);
+                                            isStillChanging = true;
+                                            if (field[row1][j].size() == 0) {
+                                                result = false;
+                                                break;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            if (field[row1][col1] == field[i][col1] && i != row1) { //if the sets are the same
+                                for (int j = 0; j < size && j != row1 && j != i; ++j) {
+                                    for (int k : field[row1][col1]) {
+                                        if (field[j][col1].find(k) != field[j][col1].end()) {
+                                            field[j][col1].erase(k);
+                                            isStillChanging = true;
+                                            if (field[j][col1].size() == 0) {
+                                                result = false;
+                                                break;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else if (field[row1][col1].size() == 3) {
+                        vector <int> intsOfIdenticalSquares;
+                        for (int i = 0; i < size; ++i) {
+                            if (field[row1][col1] == field[row1][i] && i != col1) { //if the sets are the same
+                                intsOfIdenticalSquares.push_back(i);
+                                if (intsOfIdenticalSquares.size() == 2) {
+                                    for (int j = 0; j < size && j != col1 && j != intsOfIdenticalSquares.at(1); ++j) {
+                                        for (int k : field[row1][col1]) {
+                                            if (field[row1][j].find(k) != field[row1][j].end()) {
+                                                field[row1][j].erase(k);
+                                                isStillChanging = true;
+                                                if (field[row1][j].size() == 0) {
+                                                    result = false;
+                                                    break;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            if (field[row1][col1] == field[i][col1] && i != row1) { //if the sets are the same
+                                intsOfIdenticalSquares.push_back(i);
+                                if (intsOfIdenticalSquares.size() == 3) {
+                                    for (int j = 0; j < size && j != row1 && j != i && j != intsOfIdenticalSquares.at(2); ++j) {
+                                        for (int k : field[row1][col1]) {
+                                            if (field[j][col1].find(k) != field[j][col1].end()) {
+                                                field[j][col1].erase(k);
+                                                isStillChanging = true;
+                                                if (field[j][col1].size() == 0) {
+                                                    result = false;
+                                                    break;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
         return result;
     }
-    /*
-    void doAdjustments (const int i, const int j, const int value) {
-        field[i][j].erase(value);
-        isStillChanging = true;
-        if (field[i][j].size() == 0) {
-            result = false;
-            break;
-        }
-    }
-     */
-
-
-
     virtual bool isSolution() const override {
         for (int i = 0; i < size; ++i) {
             for (int j = 0; j < size; ++j) {
@@ -141,6 +200,17 @@ public:
             }
         }
         return successors;
+    }
+    virtual int heuristicValue() const override {
+        int count = 0;
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                if(field[i][j].size() > 1) {
+                    ++count;
+                }
+            }
+        }
+        return count;
     }
 };
 #endif
